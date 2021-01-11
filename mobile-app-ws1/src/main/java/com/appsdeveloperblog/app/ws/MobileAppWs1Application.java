265af1863d10fd999bce0d3d1557c51386125917
DELL <DELL@DESKTOP-NJ3A29K>
2021-01-11 17:11:18 +0530

    added password encryption and Spring security for sign in Rest endpoint

--- mobile-app-ws1/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWs1Application.java
+++ mobile-app-ws1/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWs1Application.java
@@ -2,12 +2,19 @@ package com.appsdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MobileAppWs1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWs1Application.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- mobile-app-ws1/src/main/java/com/appsdeveloperblog/app/ws/serviceImpl/UserServiceImpl.java
+++ mobile-app-ws1/src/main/java/com/appsdeveloperblog/app/ws/serviceImpl/UserServiceImpl.java
@@ -2,17 +2,25 @@ package com.appsdeveloperblog.app.ws.serviceImpl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.io.repository.UserRepository;
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utills;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	Utills utills;
+	@Autowired
+	BCryptPasswordEncoder b;
 
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -21,9 +29,12 @@ public class UserServiceImpl implements UserService {
 
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
+		// storing the password being provided by user in encrypted format-dont store it
+		// as a plain text(Security)
 
-		userEntity.setEncryptedPassword("test");
-		userEntity.setUserId("testUserid");
+		userEntity.setEncryptedPassword(b.encode(user.getPassword()));
+		String puid = utills.generatedUserId(30);
+		userEntity.setUserId(puid);
 
 		UserEntity storedUserdetails = userRepository.save(userEntity);
 
@@ -32,6 +43,12 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(storedUserdetails, returnValue);
 
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
